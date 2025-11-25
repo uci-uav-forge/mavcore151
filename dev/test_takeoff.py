3c41b93eea07b9681cea3f0884f0fc502942bd5a
Dat-Bois <eeshvij@gmail.com>
2025-11-24 21:07:04 -0800

    added test script

---
+++ dev/test_takeoff.py
@@ -0,0 +1,41 @@
+import mavcore.mav_device as mav_device
+import mavcore.messages as messages
+import mavcore.protocols as protocols
+import time
+
+device = mav_device.MAVDevice("udp:127.0.0.1:14550")
+
+request_pos = protocols.RequestMessageProtocol(
+            messages.IntervalMessageID.LOCAL_POSITION_NED, rate_hz=30.0
+        )
+request_att = protocols.RequestMessageProtocol(
+            messages.IntervalMessageID.ATTITUDE_QUATERNION, rate_hz=30.0
+        )
+
+fp = messages.FullPose()
+device.add_listener(fp)
+
+while(True):
+    device.run_protocol(request_pos)
+    device.run_protocol(request_att)
+    time.sleep(5.0)
+    print(fp.local_position.hz)
+    if fp.local_position.hz > 25.0:
+        break
+time.sleep(2)
+set_mode_protocol = protocols.SetModeProtocol(messages.FlightMode.AUTOTUNE) # 15 is GUIDED in plane
+device.run_protocol(set_mode_protocol)
+print(f"Set GUIDED mode ack: {set_mode_protocol.ack_msg}")
+time.sleep(2)
+arm_protocol = protocols.ArmProtocol()
+device.run_protocol(arm_protocol)
+print(f"Requesting arm protocol Ack: {arm_protocol.ack_msg}")
+time.sleep(2)
+takeoff_protocol = protocols.TakeoffProtocol(
+    altitude=(20.0)
+)
+device.run_protocol(takeoff_protocol)
+while abs(fp.get_local_position().position[2] - 20.0) > 1.0:
+    print(f"Current altitude: {fp.get_local_position().position[2]}")
+    time.sleep(1)
+print("Takeoff complete")
